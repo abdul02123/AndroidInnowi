8c49b6b33e129443586173255fd565f947cc9a50
Abdul Rehman <abdul.rehman@truckerpath.com>
2023-12-25 00:40:44 +0500

    updated

--- app/src/main/java/com/innowi/android/util/network/ErrorResponse.kt
+++ app/src/main/java/com/innowi/android/util/network/ErrorResponse.kt
@@ -1,14 +1,8 @@
 package com.innowi.android.util.network
 
-import com.google.gson.annotations.SerializedName
-
 data class ErrorResponse(
-    @SerializedName("http_response")
     var httpCode: Int? = 0,
-    @SerializedName("code")
-    val code: Int? = 0,
-    @SerializedName("success")
-    val success: Boolean = false,
-    @SerializedName("message")
+    var code: Int? = 0,
+    var success: Boolean = false,
     var message: String? = ""
 )
--- app/src/main/java/com/innowi/android/util/network/ErrorResponseMapper.kt
+++ app/src/main/java/com/innowi/android/util/network/ErrorResponseMapper.kt
@@ -11,13 +11,12 @@ import retrofit2.Response
 fun <T> errorResponse(response: Response<T>): Nothing {
 
     var errorResponse = ErrorResponse()
+     errorResponse.httpCode = response.code()
     if (response.code() in 500..510) {
-        errorResponse.message =
-            BaseApplication.getInstance().getString(R.string.network_error_message_502)
+        errorResponse.message = BaseApplication.getInstance().getString(R.string.network_error_message_502)
         throw NetworkException(errorResponse)
     } else if (response.code() in 401..403) {
-        errorResponse.message =
-            BaseApplication.getInstance().getString(R.string.network_error_message_403)
+        errorResponse.message = BaseApplication.getInstance().getString(R.string.network_error_message_403)
         throw NetworkException(errorResponse)
     }
 
@@ -25,12 +24,12 @@ fun <T> errorResponse(response: Response<T>): Nothing {
         val jObjError = response.errorBody()?.string()?.let { JSONObject(it) }
         errorResponse = Gson().fromJson(jObjError.toString(), ErrorResponse::class.java)
         errorResponse.httpCode = response.code()
+        errorResponse.message = BaseApplication.getInstance().getString(R.string.network_something_wrong)
         throw NetworkException(errorResponse)
     } catch (exception: JSONException) {
         exception.printStackTrace()
         errorResponse.httpCode = response.code()
-        errorResponse.message =
-            BaseApplication.getInstance().getString(R.string.network_something_wrong)
+        errorResponse.message = BaseApplication.getInstance().getString(R.string.network_something_wrong)
         throw NetworkException(errorResponse)
     }
 }
